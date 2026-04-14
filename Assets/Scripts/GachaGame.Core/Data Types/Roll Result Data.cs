e5abddfca86d9098af61f8babcb82bc7deb20f9f
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-14 18:50:38 -0400

    Fixed Some Issues with GUIDs not deserializing

--- Assets/Scripts/GachaGame.Core/Data Types/Roll Result Data.cs
+++ Assets/Scripts/GachaGame.Core/Data Types/Roll Result Data.cs
@@ -4,11 +4,22 @@ using Newtonsoft.Json;
 [JsonObject]
 public struct RollResultData
 {
+    [JsonConstructor]
     public RollResultData(Guid characterID, Guid rarityTierID)
     {
-        CharacterID = characterID;
-        RarityTierID = rarityTierID;
+        CharacterIDHex = characterID.ToString("N").ToUpper();
+        RarityTierIDHex = rarityTierID.ToString("N").ToUpper();
     }
-    public Guid CharacterID { get; init; }
-    public Guid RarityTierID { get; init; }
+
+    [JsonProperty("CharacterID")]
+    private string CharacterIDHex { get; init; }
+
+    [JsonProperty("RarityTierID")]
+    private string RarityTierIDHex { get; init; }
+
+    [JsonIgnore]
+    public SerializableGuid CharacterID => SerializableGuid.FromHexString(CharacterIDHex);
+
+    [JsonIgnore]
+    public SerializableGuid RarityTierID => SerializableGuid.FromHexString(RarityTierIDHex);
 }
