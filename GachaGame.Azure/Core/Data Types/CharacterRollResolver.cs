2dfbafbd575505fba7e0910ccbdda6e3577919a9
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-19 23:54:44 -0400

    Revert "Implement Selection from Tier 50/50 when pulling 5 star (Needs Review)"
    This reverts commit e51017ce426261ba9ea88cb7e21bf62429b74f93.

--- GachaGame.Azure/Core/Data Types/CharacterRollResolver.cs
+++ GachaGame.Azure/Core/Data Types/CharacterRollResolver.cs
@@ -1,54 +1,21 @@
 using GachaGame.Azure.Core.Interfaces;
 using JetBrains.Annotations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GachaGame.Azure.Core.DataTypes;
-
 [UsedImplicitly]
-public struct CharacterRollResolver : IRollResolver<Character>
+public struct CharacterRollResolver: IRollResolver<Character>
 {
     public Character ResolveRoll(List<Character> possibleRolls, PlayerData playerData)
     {
-        if (possibleRolls == null || possibleRolls.Count == 0) return default;
-
+        if (possibleRolls.Count == 0) return default;
         uint ratioSum = 0;
-        foreach (var roll in possibleRolls) ratioSum += roll.Rarity;
-
-        float numericValue = (float)Random.Shared.NextDouble() * ratioSum;
-        uint targetRarity = 0;
-
-        foreach (var roll in possibleRolls)
+        foreach (Character roll in possibleRolls) ratioSum += roll.Rarity;
+        float numericValue = Random.Shared.NextSingle() * ratioSum;
+        foreach (Character roll in possibleRolls)
         {
             numericValue -= roll.Rarity;
-            if (numericValue <= 0)
-            {
-                targetRarity = roll.Rarity;
-                break;
-            }
+            if (numericValue <= 0) return roll;
         }
-
-        var tierPool = possibleRolls.Where(character => character.Rarity == targetRarity).ToList();
-
-        if (targetRarity == 5)
-        {
-            var featuredPool = tierPool.Where(character => character.IsFeatured).ToList();
-            var standardPool = tierPool.Where(character => !character.IsFeatured).ToList();
-
-            bool winFiftyFifty = playerData.HasPityCounter || Random.Shared.NextDouble() < 0.5;
-
-            if (winFiftyFifty && featuredPool.Count > 0)
-            {
-                return featuredPool[Random.Shared.Next(featuredPool.Count)];
-            }
-
-            if (standardPool.Count > 0)
-            {
-                return standardPool[Random.Shared.Next(standardPool.Count)];
-            }
-        }
-
-        return tierPool[Random.Shared.Next(tierPool.Count)];
+        return possibleRolls[^1];
     }
 }
