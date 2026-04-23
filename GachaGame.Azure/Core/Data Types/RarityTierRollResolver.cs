656cd2a3490073af7a28c1b11a20390dec4cce42
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-22 23:40:24 -0400

    Roll Resolver Closure fix and Naming Fixes

--- GachaGame.Azure/Core/Data Types/RarityTierRollResolver.cs
+++ GachaGame.Azure/Core/Data Types/RarityTierRollResolver.cs
@@ -10,78 +10,74 @@ namespace GachaGame.Azure.Core.DataTypes;
 public struct RarityTierRollResolver : IRollResolver<RarityTier>
 {
     /// <inheritdoc/>
-    public  RarityTier ResolveRoll(List<RarityTier> possibleRolls, RollContext rollContext, ILogger logger)
+    public RarityTier ResolveRoll(List<RarityTier> possibleRolls, RollContext rollContext, ILogger logger)
+{
+    uint pity = rollContext.PlayerData.BannerData.TryGetValue(rollContext.BannerName, out PlayerBannerData? bannerData) ? bannerData.CurrentPity : 0;
+
+    logger.LogInformation("Pity BEFORE: " + pity);
+    pity++;
+
+    uint rarityValue3Star;
+    uint rarityValue4Star;
+    uint rarityValue5Star;
+    if (pity >= 90)
     {
-        uint pity = 0;
-        if (rollContext.PlayerData.BannerData.TryGetValue(rollContext.BannerName, out PlayerBannerData? data))
-        {
-            pity = data.CurrentPity;
-        }
-        else
-        {
-            rollContext.TryAddMutation(playerData => playerData.BannerData[rollContext.BannerName] = new());
-        }
-        
-        logger.LogInformation("Pity BEFORE: " + pity);
-        pity++;
-        uint rarityValue3Star;
-        uint rarityValue4Star;
-        uint rarityValue5Star;
-        if(pity >= 90)
-        {
-            rarityValue5Star = 100;
-            rarityValue4Star = 0;
-            rarityValue3Star = 0;
-            pity = 0;
-        }
-        else if (pity >= 74)
-        {
-            rarityValue5Star = 25;
-            rarityValue4Star = 18;
-            rarityValue3Star = 57;
-        }
-        else if (pity >= 55)
-        {
-            rarityValue5Star = 15;
-            rarityValue4Star = 20;
-            rarityValue3Star = 65;
-        }
-        else if (pity >= 30)
-        {
-            rarityValue5Star = 10;
-            rarityValue4Star = 23;
-            rarityValue3Star = 67;
-        }
-        else
-        {
-            rarityValue5Star = 5;
-            rarityValue4Star = 25;
-            rarityValue3Star = 70;
-        }
-
-        rollContext.Banner.RarityTiers[0].Rarity = rarityValue3Star;
-        rollContext.Banner.RarityTiers[1].Rarity = rarityValue4Star;
-        rollContext.Banner.RarityTiers[2].Rarity = rarityValue5Star;
-
-        logger.LogInformation("Rarity Value 5 Star: " + rarityValue5Star);
-        logger.LogInformation("Rarity Value 4 Star: " + rarityValue4Star);
-        logger.LogInformation("Rarity Value 3 Star: " + rarityValue3Star);
-        logger.LogInformation("Pity: " + pity);
-        
-        rollContext.TryAddMutation(playerData =>
-        {
-            playerData.BannerData[rollContext.BannerName].CurrentPity = pity;
-        });
-
-        if (possibleRolls.Count == 0) return default;
-        uint ratioSum = 0;
-        foreach (RarityTier roll in possibleRolls) ratioSum += roll.Rarity;
-        float numericValue = Random.Shared.NextSingle() * ratioSum;
-        foreach (RarityTier roll in possibleRolls)
-        {
-            numericValue -= roll.Rarity;
-            if (numericValue <= 0) return roll;
-        }
-        return possibleRolls[^1];
+        rarityValue5Star = 100;
+        rarityValue4Star = 0;
+        rarityValue3Star = 0;
+        pity = 0;
     }
+    else if (pity >= 74)
+    {
+        rarityValue5Star = 25;
+        rarityValue4Star = 18;
+        rarityValue3Star = 57;
+    }
+    else if (pity >= 55)
+    {
+        rarityValue5Star = 15; 
+        rarityValue4Star = 20; 
+        rarityValue3Star = 65;
+    }
+    else if (pity >= 30)
+    {
+        rarityValue5Star = 10;
+        rarityValue4Star = 23;
+        rarityValue3Star = 67;
+    }
+    else
+    {
+        rarityValue5Star = 5; 
+        rarityValue4Star = 25; 
+        rarityValue3Star = 70;
+    }
+
+    rollContext.Banner.RarityTiers[0].Rarity = rarityValue3Star;
+    rollContext.Banner.RarityTiers[1].Rarity = rarityValue4Star;
+    rollContext.Banner.RarityTiers[2].Rarity = rarityValue5Star;
+
+    logger.LogInformation("Rarity Value 5 Star: " + rarityValue5Star);
+    logger.LogInformation("Rarity Value 4 Star: " + rarityValue4Star);
+    logger.LogInformation("Rarity Value 3 Star: " + rarityValue3Star);
+    logger.LogInformation("Pity: " + pity);
+    //Had to add this because closures in C# are passed as references
+    uint finalPity = pity;
+    rollContext.TryAddMutation(playerData =>
+    {
+        if (!playerData.BannerData.ContainsKey(rollContext.BannerName))
+            playerData.BannerData[rollContext.BannerName] = new();
+        playerData.BannerData[rollContext.BannerName].CurrentPity = finalPity;
+    });
+
+    if (possibleRolls.Count == 0) return default;
+    uint ratioSum = 0;
+    foreach (RarityTier roll in possibleRolls) ratioSum += roll.Rarity;
+    float numericValue = Random.Shared.NextSingle() * ratioSum;
+    foreach (RarityTier roll in possibleRolls)
+    {
+        numericValue -= roll.Rarity;
+        if (numericValue <= 0) return roll;
+    }
+    return possibleRolls[^1];
+}
 }
