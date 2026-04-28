4b62dd581d3fc77a858f86b91da440bbd652d2cd
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-28 17:19:43 -0400

    Documented Data Structures

--- Assets/Scripts/GachaGame.Core/Data Types/BannerRollRequestData.cs
+++ Assets/Scripts/GachaGame.Core/Data Types/BannerRollRequestData.cs
@@ -1,8 +1,13 @@
 using System;
 using UnityEngine;
-
+/// <summary>
+/// Represents the data sent to the remote playfab system in order to roll a character
+/// </summary>
 public struct BannerRollRequestData
 {
+    /// <summary>
+    /// The ID of the banner to roll
+    /// </summary>
     public string BannerID { get; }
     public BannerRollRequestData(string bannerID)
     {
--- Assets/Scripts/GachaGame.Core/Data Types/Roll Result Data.cs
+++ Assets/Scripts/GachaGame.Core/Data Types/Roll Result Data.cs
@@ -1,12 +1,29 @@
 using System;
 using Newtonsoft.Json;
-
+/// <summary>
+/// The data returned from the remote playfab server upon rolling a banner. Deserialized from JSON in the http request
+/// </summary>
 [JsonObject]
 public struct RollData
 {
+    /// <summary>
+    /// The date and time a roll occured
+    /// </summary>
     public DateTime RollTime { get; init; }
+    /// <summary>
+    /// The <see cref="Guid"/> of the character rolled
+    /// </summary>
     public Guid Character { get; init; }
+    /// <summary>
+    /// The id of the rolled banner
+    /// </summary>
     public string BannerRolled { get; init; }
+    /// <summary>
+    /// The <see cref="Guid"/> of the resulting roll's rarity tier
+    /// </summary>
     public Guid RarityTier { get; init; }
+    /// <summary>
+    /// Whether the roll succeeded or failed
+    /// </summary>
     public bool Success { get; init; }
 }
