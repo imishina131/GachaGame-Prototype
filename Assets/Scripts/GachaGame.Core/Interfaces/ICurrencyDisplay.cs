b4ce2d3e235ee9e928bab7cf0ededc293db1fdba
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-28 17:05:03 -0400

    Documented Shared Unity Interfaces

--- Assets/Scripts/GachaGame.Core/Interfaces/ICurrencyDisplay.cs
+++ Assets/Scripts/GachaGame.Core/Interfaces/ICurrencyDisplay.cs
@@ -1,9 +1,15 @@
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
-
+/// <summary>
+/// Represents a system that can take in a <see cref="CurrencyInfoSO"/> and update data from playfab about the stored currency
+/// </summary>
 public interface ICurrencyDisplay
 {
+    /// <summary>
+    /// Changes the <see cref="CurrencyInfoSO"/> displayed by this display
+    /// </summary>
+    /// <param name="currencyInfo"></param>
     void UpdateDisplayedCurrency(CurrencyInfoSO currencyInfo);
+    /// <summary>
+    /// Updates the data stored on the remote based on the current active currency in the display
+    /// </summary>
     void UpdateActiveCurrency();
 }
--- Assets/Scripts/GachaGame.Core/Interfaces/IRollHandler.cs
+++ Assets/Scripts/GachaGame.Core/Interfaces/IRollHandler.cs
@@ -1,8 +1,15 @@
-using System;
-using UnityEngine;
-
+/// <summary>
+/// Represents a system that can roll a banner
+/// </summary>
 public interface IRollHandler
 {
+    /// <summary>
+    /// Rolls the active banner
+    /// </summary>
     void Roll();
+    /// <summary>
+    /// Updates the current banner to roll
+    /// </summary>
+    /// <param name="bannerID">The new banner to roll</param>
     void UpdateBannerToRoll(string bannerID);
 }
